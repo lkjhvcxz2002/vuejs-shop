b891175ea49429084ac371ac35afa2a5881a3f41
PeterDu <lkjhvcxz2003@gmail.com>
2019-09-02 21:20:53 +0800

    Hotfix for connection str

--- server/config.js
+++ server/config.js
@@ -1,8 +1,9 @@
 var config = {}
 
 config.host = process.env.HOST || "https://2020idolchampdb.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "O8Nmif5l7cT578PgL9gJpUiKhJkuLg8ofZQNGwYOsydMJJKgWbApZbGvPBR55i3XADPbYPfbkHfjzxOUXNIUMg==";
+config.authKey = process.env.AUTH_KEY || "SoUw4KBn86t5eXPbe2cAd0WTFRX276oxYxDvEALZlbAVAJRa8RiaaOkL31Dj9i4IGlogWnHZZDVvq8GJfkK0ww==";
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
+config.ScoresColId = "Scores";
 
 module.exports = config;
