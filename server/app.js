a7a8fc641be41d8242eb64c3a7cdefbb48cd9ea6
PeterDu <lkjhvcxz2003@gmail.com>
2019-09-08 12:59:10 +0800

    Temp block add score function

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ var taskDao = new TaskDao(docDbClient, config.databaseId, config.collectionId);
 var taskList = new TaskList(taskDao);
 taskDao.init();
 // api
-app.get('/api/plua/:key/:fbId/:md5Str', taskList.addScore.bind(taskList));
+// app.get('/api/plua/:key/:fbId/:md5Str', taskList.addScore.bind(taskList));
 app.get('/api/delScore/:key/:fbId', taskList.delScore.bind(taskList));
 app.get('/api/scoreAll', taskList.scoreAll.bind(taskList));
 app.post('/api/updateData/:pa', taskList.updateData.bind(taskList))
