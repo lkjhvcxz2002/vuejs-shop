826330d75f87607a5512bb7160d91c43e725e1fa
Peter <lkjhvcxz2003@gmail.com>
2019-09-12 17:53:41 +0800

    Update order

--- src/components/score/ScoreList.vue
+++ src/components/score/ScoreList.vue
@@ -72,7 +72,7 @@
             <td>{{idol.group}}</td>
             <td>
               <div class="ui fluid image">
-                <span class="ui pink label ribbon lableText" v-if="idol.isEnRolled">獲選，第 {{resultJ['enroll'].indexOf(parseInt(idol.key)) + 1}} 位</span> 
+                <span class="ui pink label ribbon lableText" v-if="idol.isEnRolled">恭喜獲選，第 {{resultJ['enroll'].indexOf(parseInt(idol.key)) + 1}} 位</span> 
                 <span class="ui orange label ribbon lableText" v-if="idol.isExcluded">重複獲選，資格遞延</span> 
                 <a :href="'/resultPage/' + idol.key"><img :src="idol.thumb" class="card-img p-3"/></a>
               </div>
